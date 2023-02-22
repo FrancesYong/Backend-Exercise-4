59c75aba2fb0339e5978dee6824d45cae406b1fe
Frances Rully Yong <frances.yong080900@gmail.com>
2023-02-22 14:32:14 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const moment = require('moment')
+const member = require('./members')
+const user = require('./users')
+
+app.get("/", (req, res) =>{
+    res.send(`This is the home page`);
+});
+
+app.get("/about", (req, res) =>{
+    res.write(JSON.stringify({
+        'Status': 'success',
+        'Message': 'response success',
+        'Description': 'Exercise #3',
+        'Date': moment().format(),
+        'Data': member.data()
+    }));
+    res.end();
+});
+
+app.get("/users", (req, res) =>{
+    res.write(JSON.stringify(
+        user.user()
+    ))
+    res.end();
+})
+
+app.listen(port, ()=>{
+    console.log(`Server running at http://localhost:${port}`);
+});
---
+++ members.js
@@ -0,0 +1,37 @@
+module.exports.data = () => [
+    {
+        "id": 0,
+        "nim": "001",
+        "first_name": "Vicky",
+        "last_name": "Pati Ani",
+        "phone": "0812 XXXX XXXX"
+    },
+    {
+        "id": 2,
+        "nim": "002",
+        "first_name": "Prety",
+        "last_name": "Lamalo",
+        "phone": "0822 XXXX XXXX"
+    },
+    {
+        "id": 3,
+        "nim": "003",
+        "first_name": "Cherlita",
+        "last_name": "Patuli",
+        "phone": "0813 XXXX XXXX"
+    },
+    {
+        "id": 4,
+        "nim": "004",
+        "first_name": "Joseph",
+        "last_name": "Tiwow",
+        "phone": "0823 XXXX XXXX"
+    },
+    {
+        "id": 5,
+        "nim": "005",
+        "first_name": "Frances",
+        "last_name": "Yong",
+        "phone": "0823 XXXX XXXX"
+    }
+]
